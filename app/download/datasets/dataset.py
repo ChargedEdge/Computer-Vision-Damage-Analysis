144ef67fd84c1bbd35ce52695992cb6e2b997da3
swentyl <tylerjswenson262@gmail.com>
2025-04-14 03:55:10 -0500

    Changed how the datasets are saved to work better with torch utils

--- app/download/datasets/dataset.py
+++ app/download/datasets/dataset.py
@@ -13,8 +13,9 @@ def move_dataset(download_dir, model_data_dir, class_name):
     training_imgs, validation_imgs = train_test_split(images, train_size=0.8, test_size=0.2, random_state=42)
 
     # Create the output folders
-    train_dir = os.path.join(model_data_dir, class_name, "train")
-    validation_dir = os.path.join(model_data_dir, class_name, "validation")
+    train_dir = os.path.join(model_data_dir, "train", class_name)
+    validation_dir = os.path.join(model_data_dir, "validation", class_name)
+    
     os.makedirs(train_dir, exist_ok=True)
     os.makedirs(validation_dir, exist_ok=True)
 
